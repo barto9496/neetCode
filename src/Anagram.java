40d7763aae14c8f935045068249c329f568ca74a
barto9496 <amoghkatti9496@gmail.com>
2024-10-28 21:19:47 -0500

    added two sum array problem

--- src/Anagram.java
+++ src/Anagram.java
@@ -3,19 +3,21 @@ import java.util.Map;
 
 public class Anagram {
     public boolean isAnagram(String s, String t) {
-         if (s.length() != t.length()) {
-             return false;
-         }
-         Map<Character, Integer> charCount = new HashMap<>();
-         for(char c: s.toCharArray()) {
-             charCount.put(c, charCount.getOrDefault(c, 0) + 1);
-         }
+        Map<Character,Integer> map = new HashMap<>();
+        if(s.length() != t.length()) return false;
 
-         for(char c: t.toCharArray()) {
-             if(!charCount.containsKey(c) || charCount.get(c) == 0) {
-                 return false;
-             }
-         }
-         return true;
+        for(char ch: s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1 );
+        }
+
+        for(char ch: t.toCharArray()){
+            if(!map.containsKey(ch) || map.get(ch) == 0){
+                return false;
+            }
+
+            map.put(ch, map.get(ch) - 1);
+
+        }
+        return true;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        DuplicateInteger duplicateInteger = new DuplicateInteger();
-        int[] ar = new int[4];
-        ar[0] = 1;
-        ar[1] = 2;
-        ar[2] = 2;
-        ar[3] = 3;
-        System.out.println(duplicateInteger.hasDuplicate(ar));
+//        System.out.println("Hello world!");
+//        DuplicateInteger duplicateInteger = new DuplicateInteger();
+//        int[] ar = new int[4];
+//        ar[0] = 1;
+//        ar[1] = 2;
+//        ar[2] = 2;
+//        ar[3] = 3;
+//        System.out.println(duplicateInteger.hasDuplicate(ar));
+//
+//        // ************************************* Anagram ******************************************* //
+//        Anagram anagram = new Anagram();
+//        String str1 = "listen";
+//        String str2 = "silent";
+//        System.out.println("Final anagram details: " + anagram.isAnagram(str1,str2));
 
-        // ************************************* Anagram ******************************************* //
-        Anagram anagram = new Anagram();
-        String str1 = "listen";
-        String str2 = "silent";
-        System.out.println("Final anagram details: " + anagram.isAnagram(str1,str2));
+        TwoSum twosum = new TwoSum();
+        int[] arr = new int[5];
+        arr[0] = 5;
+        arr[1] = 2;
+        arr[2] = 7;
+        arr[3] = 9;
+        arr[4] = 120;
+        int [] res = twosum.twoSum(arr, 14);
+        System.out.println("Final result" + Arrays.toString(res));
     }
 }
 
